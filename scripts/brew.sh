b5d992c34630962031bd6fc3f2fe3226565daa20
Chris Svenningsen <crsven@gmail.com>
2023-11-10 13:05:34 -0800

    Use new brew URL.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,6 +1,6 @@
 if ! command -v brew >/dev/null; then
   fancy_echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH="/usr/local/bin:$PATH"
 else
   fancy_echo "Homebrew already installed. Skipping ..."

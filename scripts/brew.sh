3d507b4a952d639c8e5dd822bfa69d6f19ea5594
Chris Svenningsen <crsven@gmail.com>
2023-11-10 13:08:26 -0800

    Use bash to install brew.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,6 +1,6 @@
 if ! command -v brew >/dev/null; then
   fancy_echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   export PATH="/usr/local/bin:$PATH"
 else
   fancy_echo "Homebrew already installed. Skipping ..."
